cd5bfae3474cf521acf14c9c00ea9acc0eed923d
Agada <agadap46@gmail.com>
2022-09-21 19:35:06 +0000

    Created Basic Compoments for Project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+   <React.Fragment>
+    <Header/>
+    <Nav/>
+    <About/>
+    <Experience/>
+    <Portfolio/>
+    <Contact/>
+    <Footer/>
+   </React.Fragment>
+  );
+}
+
+export default App;
